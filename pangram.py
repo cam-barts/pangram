70e99637640729bc7d7a38959fdc816f2cd21c4a
ERAU MSSA Student <wwstudent@cam15.CP10.MSSA>
2018-05-21 10:28:19 -0700

    Initial commit

---
+++ pangram.py
@@ -0,0 +1,14 @@
+def is_pangram(sentence):
+    for i in "abcdefghijklmnopqrstuvwxyz":
+        if i not in sentence.lower():
+            return False
+    return True
+
+# #boolen bullshit
+# test = "qwertyuiolkjhgfdsazxcvvbnm"
+# def panderin(test):
+#     for i in "qwertyuiopasdfghjklzxcvbnm":
+#         if i not in sentence:
+#             print(i)
+#             return False
+#     return True
